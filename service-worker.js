4a41f0004ea4a45fc01261523741809fa95c5f1c
Camille1111 <53229974+ChangeSuger@users.noreply.github.com>
2022-10-19 12:39:40 +0800

    delete sw

--- service-worker.js
+++ service-worker.js
@@ -235,3 +235,12 @@ addEventListener('message', event => {
     )
   }
 })
+caches.keys()
+  .then(keys =>
+    Promise.all(
+      keys.map(async key => console.log("caches.delete", key, await caches.delete(key)))))
+  .then(async () => {
+    console.log("registration.unregister", await registration.unregister());
+  })
+  .then(() => console.log("DONE"))
+  .catch(console.error);
